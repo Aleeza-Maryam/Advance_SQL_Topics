fc02a540c2667e7ad05d483c2cd52dc765fbfd73
Aleeza-Maryam <aleezamarryam563@gmail.com>
2026-07-15 22:22:03 +0500

    Right join

--- outer_join.sql
+++ outer_join.sql
@@ -5,3 +5,18 @@ FROM Customers
 LEFT JOIN Orders 
 ON Customers.Customer_ID = Orders.OrderID
 ORDER BY Customers.Customer_name;
+
+
+//To find only the customers who have not placed any order WHERE clause to filter for NULL values on the right table
+
+SELECT Customers.CustomerName, Orders.OrderID
+FROM Customers
+LEFT JOIN Orders
+ON Customers.CustomerID = Orders.CustomerID
+WHERE Orders.CustomerID IS NULL;
+
+//RIGHT JOIN
+SELECT Orders.OrderID, Employees.LastName, Employees.FirstName
+FROM Orders
+RIGHT JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+ORDER BY Orders.OrderID;

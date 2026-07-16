31887f700ab32763c28295305c00098448302bde
Aleeza-Maryam <aleezamarryam563@gmail.com>
2026-07-16 23:18:45 +0500

    Self join

--- full_join.sql
+++ full_join.sql
@@ -5,3 +5,10 @@ FROM CUSTOMERS
 FULL JOIN Orders
 ON Customers.Customer_ID=Orders.Customer_ID;
 
+//SELF JOIN
+
+SELECT A.Customer_name AS CustomerName1 , B.Customer_name AS CustomerName2, A.City
+FROM Customer A, Customer B
+WHERE A.Customer_ID<>B.Customer_ID
+AND A.City=B.City
+ORDER BY A.City;

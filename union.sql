bb18a4e13b18b8437507d8a8aa73d98ba1c54735
Aleeza-Maryam <aleezamarryam563@gmail.com>
2026-07-17 23:25:46 +0500

    UNION

--- union.sql
+++ union.sql
@@ -6,3 +6,20 @@ SELECT Country FROM Customers
 UNION 
 SELECT Country FROM Suppliers
 ORDER BY Country;
+
+//UNION With WHERE
+
+SELECT City,Country FROM Customer
+WHERE Country='Germany'
+UNION
+SELECT City,Country FROM Suppliers
+WHERE Country='Germany'
+ORDER BY City;
+
+
+//AS Type  aik label bna de ga Customer / Supplier ka hrr column k start mai likha ho ga
+SELECT 'Customer' AS Type, ContactName, City, Country
+FROM Customers
+UNION
+SELECT 'Supplier', ContactName, City, Country
+FROM Suppliers;

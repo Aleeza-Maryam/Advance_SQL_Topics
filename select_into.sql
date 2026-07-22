3090adac8929bf52d0a7e798b3282462ce8c0c76
Aleeza-Maryam <aleezamarryam563@gmail.com>
2026-07-22 23:08:06 +0500

    SELECT INTO

--- select_into.sql
+++ select_into.sql
@@ -17,3 +17,9 @@ SELECT Customers.Customer_name , Orders.OrderDetails INTO new_table
 FROM Customers
 LEFT JOIN  Orders
 WHERE Customers.Customer_ID=Orders.Customer_ID;
+
+
+//"Existing table (sourcetable) jaisa ek naya khali (empty) table newtable ke naam se banana, jis mein sirf structure/columns copy hon par koi data copy na ho."
+SELECT * INTO newtable
+FROM sourcetable
+WHERE 1 = 0;  //ye hmesha false rhe gi isliye koi row ni copy honi

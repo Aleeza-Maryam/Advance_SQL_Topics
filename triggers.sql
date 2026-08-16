164dc92c6079eea33da41bc9bef29aeb19943ad7
Aleeza-Maryam <aleezamarryam563@gmail.com>
2026-08-16 23:52:40 +0500

    triggers

--- triggers.sql
+++ triggers.sql
@@ -12,3 +12,31 @@ BEGIN
 END //
 
 DELIMITER ;
+
+
+//Hum chahte hain salary change hone par history save ho.
+
+DELIMITER //
+
+CREATE TRIGGER after_salary_update
+AFTER UPDATE
+ON Employees
+FOR EACH ROW
+BEGIN
+
+    INSERT INTO SalaryHistory
+    (
+        EmployeeID,
+        OldSalary,
+        NewSalary
+    )
+    VALUES
+    (
+        OLD.ID,
+        OLD.Salary,
+        NEW.Salary
+    );
+
+END //
+
+DELIMITER ;
